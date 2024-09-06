27563ba0a34e99ba32a05347b3960d798574dc24
Nicholas DeMarinis <ndemarinis@wpi.edu>
2024-09-06 19:30:40 -0400

    Added source after lecture.

--- src/Dillo.java
+++ src/Dillo.java
@@ -8,4 +8,24 @@ public class Dillo {
         this.length = l;
         this.isDead = isD;
     }
+
+    /**
+     * Write a function to determine whether a Dillo
+     * is larger than 60 units and dead
+     * boolean canShelter(Dillo d) {
+     *     d.length > 60 && d.isDead
+     * }
+     */
+    // everything, including functions, must be in a class
+    // we call this a  **method**
+    public boolean canShelter() {
+        return (this.length > 60) && (this.isDead == true);
+    }
+
+    // Return true if this dillo is larger than another
+    public boolean isBigger(Dillo other) {
+        return this.length > other.length;
+    }
+
+
 }
--- test/AnimalsTest.java
+++ test/AnimalsTest.java
@@ -10,7 +10,15 @@ import src.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class AnimalsTest {
 
-    Dillo babyDillo = new Dillo(5,false);
+    Dillo babyDillo = new Dillo(5, false);
+    // make a new Dillo, call it babyDillo
+
+    // Make some more Dillos
+    Dillo d1 = new Dillo(20, true);
+    Dillo d2 = new Dillo(20000, true);
+
+    Bear bear1 = new Bear(50);
+
     /**
      * Example test method.
      * Test methods must have @Test before the method
@@ -20,8 +28,21 @@ public class AnimalsTest {
         // example syntax for assertEquals
         Assert.assertEquals(2, 1 + 1);
     }
+
     @Test
     public void testMakeDillo() {
         Assert.assertEquals(5, babyDillo.length);
     }
+
+    @Test
+    public void testCanShelter() {
+        Assert.assertEquals(false, babyDillo.canShelter());
+    }
+
+    @Test
+    public void testIsBigger() {
+        Assert.assertEquals(true, d2.isBigger(d1));
+        Assert.assertEquals(false, d1.isBigger(d2));
+    }
+
 }
